15c8140d5aba66286e251e07535ed8c0f01f91bb
rachitdalal21 <Rachit1@3>
2020-03-10 18:00:28 -0600

    ps1 - Removed unused import - changed MEGABYTE and KILOBYTE constants to make sure split should not exceeds the size of the pages.

--- pdfsam-split-by-size/src/main/java/org/pdfsam/splitbysize/SizeUnit.java
+++ pdfsam-split-by-size/src/main/java/org/pdfsam/splitbysize/SizeUnit.java
@@ -17,7 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.pdfsam.splitbysize;
-import java.math.BigDecimal;
 
 import org.pdfsam.i18n.DefaultI18nContext;
 
