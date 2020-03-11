b817dfd97a8a5ce63cfbb32078393e85325a7fbd
rachitdalal21 <Rachit1@3>
2020-03-10 18:44:52 -0600

    ps1 added import

--- pdfsam-split-by-size/src/main/java/org/pdfsam/splitbysize/SizeUnit.java
+++ pdfsam-split-by-size/src/main/java/org/pdfsam/splitbysize/SizeUnit.java
@@ -17,6 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.pdfsam.splitbysize;
+import java.math.BigDecimal;
 
 import org.pdfsam.i18n.DefaultI18nContext;
 
